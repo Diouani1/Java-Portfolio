b64f749d22ab2164ae7ced4b28aa9b3c635f941f
Diouani <diouani.mokhtar@outlook.de>
2025-07-10 10:08:19 +0200

    Add local.properties loading and update mail config

--- src/main/java/j/project/portfolio/PortfolioApplication.java
+++ src/main/java/j/project/portfolio/PortfolioApplication.java
@@ -3,11 +3,29 @@ package j.project.portfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 @SpringBootApplication
 public class PortfolioApplication {
 
 	public static void main(String[] args) {
+		// Load from local.properties if available
+		try {
+			Properties localProps = new Properties();
+			FileInputStream fis = new FileInputStream("local.properties");
+			localProps.load(fis);
+			fis.close();
+
+			for (String key : localProps.stringPropertyNames()) {
+				System.setProperty(key, localProps.getProperty(key));
+			}
+
+			System.out.println("✅ Loaded local.properties.");
+		} catch (Exception e) {
+			System.out.println("⚠️ Could not load local.properties: " + e.getMessage());
+		}
+
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
-
 }
